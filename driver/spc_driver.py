69fe2b7d46257c71594e2d5193676d79959333d5
ralds <ronald.ralds@gmail.com>
2023-05-19 15:40:38 -0300

    add bot telegram

---
+++ driver/spc_driver.py
@@ -0,0 +1,68 @@
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import time
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.expected_conditions import (
+    alert_is_present,
+)
+
+
+class Spc:
+    def __init__(self, 
+                 operation: str, 
+                 password: str, 
+                 secret: str, 
+                 url: str,
+                 headless: bool = False,
+                 ):
+        self._operation: str = operation
+        self._password: str = password
+        self._secret: str = secret
+        options = webdriver.ChromeOptions()
+        prefs = {
+            "download.default_directory": "",
+            "download.prompt_for_download": False,
+            "download.directory_upgrade": True,
+            "safebrowsing.enabled": True
+        }
+        options.add_experimental_option("prefs", prefs)
+        options.headless = headless
+        self._driver = webdriver.Chrome(
+            service=Service(ChromeDriverManager().install()),
+            options=options
+            )
+        self._wdw = WebDriverWait(self._driver, 600)
+        self._mouse = ActionChains(self._driver)
+        self._driver.get(url)
+
+    def click(self, xpath: str):
+        self._mouse.move_to_element(self._driver.find_element(
+            By.XPATH, xpath
+        )).pause(5).click().pause(5).perform()
+
+    def write(self, xpath: str, text: str):
+        self._mouse.move_to_element(self._driver.find_element(
+            By.XPATH, xpath
+        )).pause(2).click().send_keys(text).pause(2).perform()
+
+    def text(self, xpath: str) -> str:
+        value = self._driver.find_element(By.XPATH, xpath).get_attribute("value")
+        return value
+
+    def include(self):
+        self._wdw.until(alert_is_present())
+        self._driver.switch_to.alert.accept()
+        time.sleep(5)
+
+    def login(self):
+        self.write('//*[@id="j_username"]', self._operation)
+        self.write('//*[@id="j_password"]', self._password)
+        self.click('//*[@id="submitButton"]')
+        self.write('//*[@id="passphrase"]', self._secret)
+        self.click('//*[@id="submitButton"]')
+
+    def close(self):
+        self._driver.close()
---
+++ process/spc.py
@@ -0,0 +1,90 @@
+import time
+from driver.spc_driver import Spc
+from selenium.webdriver.common.keys import Keys
+from dotenv import load_dotenv
+import os
+import logging
+from datetime import datetime
+
+load_dotenv()
+
+file_log = datetime.now().strftime("spc_include_%d_%m_%Y.log")
+logging.basicConfig(
+    filename=os.path.join(os.path.dirname(__file__), 'logs', file_log),
+    encoding='utf-8',
+    filemode='a',
+    format='%(levelname)s - %(asctime)s - %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p',
+    level=logging.WARNING
+)
+
+SUCESS = 35
+logging.addLevelName(SUCESS,'SUCESS')
+
+def include(
+        cpf_cnpj,
+        data_nascimento,
+        ddd,
+        celular,
+        cep,
+        logradouro,
+        numero,
+        complemento,
+        bairro,
+        data_vencimento,
+        data_compra,
+        cod_cliente,
+        valor_debito
+):
+    print(
+        f'cadastrando {cpf_cnpj}')
+    try:
+        spc = Spc(
+            url=os.getenv('URL_SPC'),
+            operation=os.getenv('OPERATION_SPC'),
+            password=os.getenv('PASSWORD_SPC'),
+            secret=os.getenv('SECRET_KEY_SPC')
+        )
+
+        spc.login()
+        spc.click('//*[text()="Inclusão/Exclusão"]')
+        spc.click('//a[@class="menu-principal"]')
+        spc.click('//a[@class="sub-menu"]')
+        spc.click('//*[@name="SPC (INC/EXC) - Novo"]')
+        spc.write('//*[@id="consumidorPessoaFisica.cpf"]', cpf_cnpj + Keys.TAB)
+        time.sleep(5)
+        if spc.text('//*[@id="consumidorPessoaFisica.nome"]') == 'CADASTRO NAO LOCALIZADO':
+            msgCadastro = f'CPF: {cpf_cnpj} - cadastro nao localizado'
+            logging.warning(msgCadastro)
+            spc.close()
+            return
+
+        if spc.text('//*[@id="consumidorPessoaFisica.dataNascimento"]') == '':
+            spc.write(
+                '//*[@id="consumidorPessoaFisica.dataNascimento"]', data_nascimento + Keys.TAB)
+        spc.write(
+            '//*[@id="consumidorPessoaFisica.telefones0.numeroDDD"]', ddd)
+        spc.write(
+            '//*[@id="consumidorPessoaFisica.telefones0.numero"]', celular)
+        spc.write(
+            '//input[@id="consumidorPessoaFisica.enderecos0.cep"]', cep + Keys.TAB)
+        spc.write(
+            '//*[@name="consumidorPessoaFisica.enderecos[0].logradouro"]', logradouro)
+        spc.write(
+            '//*[@id="consumidorPessoaFisica.enderecos0.numero"]', numero)
+        spc.write(
+            '//*[@id="consumidorPessoaFisica.enderecos0.complemento"]', complemento)
+        spc.write(
+            '//*[@id="consumidorPessoaFisica.enderecos0.bairro"]', bairro)
+        spc.write('//*[@id="dataVencimento"]', data_vencimento + Keys.TAB)
+        spc.write('//*[@id="dataCompra"]', data_compra + Keys.TAB)
+        spc.write('//*[@id="numeroContrato"]', cod_cliente + Keys.TAB)
+        spc.write('//*[@id="valorDebito"]', valor_debito + Keys.TAB)
+        spc.include()
+        msgSucess = f'CPF: {cpf_cnpj} - Cadastrado com sucesso'
+        logging.log(SUCESS, msgSucess)
+        print(msgSucess)
+        spc.close()
+    except:
+        msgErro = f'CPF: {cpf_cnpj} - Erro no cadastro do cliente'
+        logging.error(msgErro)
